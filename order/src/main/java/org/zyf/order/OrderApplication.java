4aedb71e7186af19e21dedddc751342308feb8b8
zhangfeifan21 <1426107672@qq.com>
2024-04-12 17:09:27 +0100

    Nacos Discovery

--- order/src/main/java/org/zyf/order/OrderApplication.java
+++ order/src/main/java/org/zyf/order/OrderApplication.java
@@ -3,9 +3,11 @@ package org.zyf.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @MapperScan("org.zyf.order.dao")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderApplication {
 
     public static void main(String[] args) {
--- product/src/main/java/org/zyf/product/ProductApplication.java
+++ product/src/main/java/org/zyf/product/ProductApplication.java
@@ -3,6 +3,7 @@ package org.zyf.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 整合MyBatis Plus
@@ -13,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan("org.zyf.product.dao")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductApplication {
 
 	public static void main(String[] args) {
